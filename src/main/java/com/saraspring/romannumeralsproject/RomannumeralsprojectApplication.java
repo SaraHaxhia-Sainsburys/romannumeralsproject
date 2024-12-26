3fa61fe714271dd9c382426ea01bf47ac12d2f17
Sara Haxhia <sara.haxhia@sainsburys.co.uk>
2024-12-26 11:44:23 +0000

    First commit

---
+++ src/main/java/com/saraspring/romannumeralsproject/RomannumeralsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.saraspring.romannumeralsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomannumeralsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomannumeralsprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saraspring/romannumeralsproject/RomannumeralsprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saraspring.romannumeralsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomannumeralsprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
